fb38ffbbc1f858c47bfbf20a4f7c60f460f77bf8
Shinichi <miyakawashi@gmail.com>
2022-06-26 18:04:25 -0400

    implement structure to contentScript

---
+++ js/contentScript.js
@@ -0,0 +1,10 @@
+const test = document.querySelectorAll("a");
+
+console.log(test);
+// loop through list of anchor tags, compare the text content of each anchor tag with the associated href contained in HTML element
+// if textContent does not match any string in HTML href, most likely spam or phishing
+//  highlight element RED
+// else
+// do nothing
+
+console.log("testing for contents");
